bc612f4a1ab45c75d33b3b00e1c6a878f0716ce0
stevehighly0 <stevehighly0>
2023-01-08 20:21:25 +0300

    step 8

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+// Generic Exceptions
+class GenericAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
+
+// Login Exceptions
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+// Register Exceptions
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
